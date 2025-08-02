8c9672f6d702c29bb02487454bea5640275d1b3b
suryabhukya24 <suryabhukya2@gmail.com>
2025-08-02 11:32:42 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Athletes from "./components/Athletes";
 import ProfileForm from "./pages/ProfileForm";
-import HomePage from "./pages/HomePage"; // ✅ Make sure this exists and is imported
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} /> {/* your main homepage */}
         <Route path="/submit-profile" element={<ProfileForm />} />
       </Routes>
     </Router>
   );
 }
-
-export default App; // ✅ This line is required
+export default App
