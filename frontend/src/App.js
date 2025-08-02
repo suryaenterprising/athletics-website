d3ece9f34c472ba13b91961e9c49dc1610ff662e
suryabhukya24 <suryabhukya2@gmail.com>
2025-08-02 12:29:15 +0530

    final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Athletes from "./components/Athletes";
 import ProfileForm from "./pages/ProfileForm";
-import Home from "./pages/Home"; 
+import HomePage from "./pages/HomePage"; // 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/submit-profile" element={<ProfileForm />} />
       </Routes>
     </Router>
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div id="home">
+      <h1>Welcome to the Athletics Website</h1>
+      <p>This is the homepage. Add your content here.</p>
+    </div>
+  );
+};
+
+export default HomePage;
