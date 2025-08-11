21266994108f250418d575c2662f34b80fdd75b2
suryabhukya24 <suryabhukya2@gmail.com>
2025-08-11 19:32:30 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,10 @@ app.use(
       }
     },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
-
 app.options(/^.*$/, cors());
 
 /**
